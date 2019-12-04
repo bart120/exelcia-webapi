93e43cdde478815a846a77ab4e7a4f839d017b3d
bart120 <vleclerc@inow.fr>
2019-12-04 14:57:27 +0400

    correction swagger

--- src/main/java/com/exelcia/webapi/WebapiApplication.java
+++ src/main/java/com/exelcia/webapi/WebapiApplication.java
@@ -7,9 +7,12 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import com.exelcia.webapi.controller.ForfaitController;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
 @EnableJpaAuditing
 @ComponentScan(basePackageClasses = ForfaitController.class)
+@EnableSwagger2
 public class WebapiApplication {
 
 	public static void main(String[] args) {
